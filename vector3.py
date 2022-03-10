c85fcf9bd19d5757b68dab3c4b8396cf6fc3bdbe
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-03-10 20:56:10 +0100

    Add files via upload

--- vector3.py
+++ vector3.py
@@ -20,6 +20,19 @@ class vector3:
         values = [self.x1, self.x2, self.x3]
         print(values)
 
+    def unit(self):
+        """
+        calculates the unit Vector
+
+        :param vA: vector A
+        :return: unit vector of A (return vector has len 1)
+        """
+        vectorLen = sumVector3(self)
+        multi = 1 / vectorLen
+        print(multi)
+        
+        return multiplyVector3(self, multi)
+
     def invert(self):
         """
         inverts the current vector, does not change the vector it self
@@ -354,19 +367,6 @@ def point3plane(vA, vB, vC):
     plane3 = plane3param(supportV, clampA, clampB)
 
     return plane3
-
-def unitVector3(vA):
-    """
-    calculates the unit Vector
-
-    :param vA: vector A
-    :return: unit vector of A (return vector has len 1)
-    """
-    vectorLen = sumVector3(vA)
-    multi = 1 / vectorLen
-    print(multi)
-    return multiplyVector3(vA, multi)
-
 
 
 def compareLines(g1, g2):
