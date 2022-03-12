b47156bb128d1f8994d2b38401fa974c795e4dc8
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2022-03-12 14:42:51 +0100

    Add files via upload

---
+++ vector3/__init__.py
@@ -0,0 +1,25 @@
+__version__ = "1.5.2"
+
+import math
+from .vector_math import (midVector3 as midVector3,
+                         point3plane,
+                         compareLines,
+                         sumVector3,
+                         multiplyVector3,
+                         calcVector3,
+                         addVector3,
+                         dotProduct,
+                         crossProduct,
+                          line3,
+                          plane3param,
+                          plane3coord,
+                          plane3normal,
+                          plane3,
+                          vector3
+                         )
+
+"""
+credits to Bastian Lipka
+-
+A lib for calculations with vectors (3d) 
+"""
