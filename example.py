9c67bc526d468f73780d3e353185461fe7793ed5
Bastian Lipka <68615019+kirbs-btw@users.noreply.github.com>
2024-04-03 13:00:21 +0200

    Add files via upload

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import vector3 as vm
 
+
 def main():
     vectorA = [1, 2, 3]
     vectorB = [0, 2, 1]
@@ -14,7 +15,33 @@ def main():
     print(vm.crossProduct(vectorA, vectorB))
     
     
+
+def testLine():
+    # test of the things
+    """
+    v1g1 = [100.19999999999999, 50, 49.8]
+    v2g1 = [50, 50, 49.8]
     
+    v1g2 = [0.5, 0, 49.8]
+    v2g2 = [1.5, 1, 49.8] 
+    
+    a = ComplexLine(v1g1, v2g1)
+    b = ComplexLine(v1g2, v2g2)
+    
+    print(intersection(a, b))
+    
+    """
+
+    # testing the current line 
+    gaS = [100.2, 50.0, 49.8]
+    gaD = [-50.2, 0.0, 0.0]
+
+    gbS = [0.5, 0, 49.8]
+    gbD = [1, 1, 0]
+    a = vm.Line(gaS, gaD)
+    b = vm.Line(gbS, gbD)
+    print(vm.intersection(a, b))
 
 if __name__ == '__main__':
-    main()
+    testLine()
+    # main()
